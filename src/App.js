ca269f5e0140cde5e32191b068db77ea5268a831
Sahas Ramesh <>
2024-01-18 20:10:17 +0530

    made it way way better

--- src/App.js
+++ src/App.js
@@ -1,64 +1,74 @@
 import * as React from 'react';
-import Select from '@mui/joy/Select';
-import Option from '@mui/joy/Option';
-import Switch from '@mui/joy/Switch';
-
-import Icon from '@mdi/react';
-import { mdiFilter } from '@mdi/js';
-import { mdiSort } from '@mdi/js';
-
+import { CssVarsProvider, extendTheme } from "@mui/joy/styles";
+import FormControl from '@mui/joy/FormControl';
+import FormHelperText from '@mui/joy/FormHelperText';
+import Input from '@mui/joy/Input';
+import Chip from '@mui/joy/Chip';
+import SearchIcon from '@mui/icons-material/Search';
 import Korvai from './components/Korvai';
+import { korvai1, korvai2, korvai3, korvai4, korvai5, korvai6 } from './korvais';
+import { description1, description2, description3, description4, description5, description6 } from './descriptions';
 
 export default function App() {
-  const [checked, setChecked] = React.useState(true);
-  const example1 = 'grsnd,grsndrsndp,grsndrsndpsndpmgmpdp,gmpdpmpdnd,gmpdpmpdndpdnsrgrsnd,grsndrsndp,grsndrsndpsnpdn';
-  const example2 = 'g,,,r,,,s,,,n,,,d,,,g,,r,,s,,n,,d,,g,r,s,n,d,grsndn,rsndpd,sndpmg,,,m,,,p,,,d,,,p,,,g,,m,,p,,d,,p,,g,m,p,d,p,gmpdpd,mpdndn,pdnsrg,,,r,,,s,,,n,,,d,,,g,,r,,s,,n,,d,,g,r,s,n,d,grsndn,rsndpd,snpdn';
-  const example3 = 'g,r,s,,gr,s,,grs,,rs,,s,,grsndrsndpsndpmg,m,p,,gm,p,,gmp,,mp,,p,,gmpdpmpdndpdnsrg,r,s,,gr,s,,grs,,rs,,s,,grsndrsndpsnpdn';
+  const [searchText, setSearchText] = React.useState('');
+
+  const customTheme = extendTheme({
+    fontFamily: {
+      body: 'Be Vietnam Pro',
+    },
+  });
+
+  const korvais = [
+    { letters: korvai1, talam: 'adi', ragam: 'sankarabharanam', description: description1 },
+    { letters: korvai2, talam: 'khanda chapu', ragam: 'gowri manohari', description: description2 },
+    { letters: korvai3, talam: 'khanda chapu', ragam: 'latangi', description: description3 },
+    { letters: korvai4, talam: 'adi', ragam: 'kharaharapriya', description: description4 },
+    { letters: korvai5, talam: 'adi', ragam: 'kaapi', description: description5 },
+    { letters: korvai6, talam: 'adi', ragam: 'abheri', description: description6 },
+  ];
+
+  const filteredKorvais = korvais.filter(korvai =>
+    korvai.letters.includes(searchText) ||
+    korvai.talam.includes(searchText) ||
+    korvai.ragam.includes(searchText) ||
+    korvai.description.includes(searchText)
+  );
 
   return (
+    <CssVarsProvider theme={customTheme}>
     <div className="bg-slate-300">
+      <div className='text-4xl font-viet font-bold text-center mt-16 mb-8'>Korvais Collection</div>
       <div className="flex justify-center">
-        <div className="m-4 bg-zinc-50 rounded-3xl w-3/4 drop-shadow-lg">
-          <div className="flex items-center gap-10 p-6 flex-wrap">
-            <div className="flex items-center gap-4">
-              <Icon path={mdiFilter} size={1} />
-              <Select>
-                <Option value="adi">Adi</Option>
-                <Option value="khanda">Khanda Chapu</Option>
-                <Option value="misra">Misra Chapu</Option>
-                <Option value="rupakam">Rupakam</Option>
-              </Select>
-            </div>
-            <div className="flex items-center gap-4">
-              <Icon path={mdiSort} size={1} />
-              <Select>
-                <Option value="author">Author</Option>
-                <Option value="date">Date</Option>
-                <Option value="length">Length</Option>
-              </Select>
-            </div>
-            <div className="flex items-center gap-4">
-              <Switch
-                checked={checked}
-                onChange={(event) => setChecked(event.target.checked)}
-                color='neutral'
-                variant='outlined'
-                endDecorator={checked ? 'Ascending' : 'Descending'}
-                slotProps={{
-                  endDecorator: {
-                    sx: {
-                      minWidth: 24,
-                    },
-                  },
-                }}
+        <div className="m-4 bg-zinc-50 rounded-3xl drop-shadow-lg">
+          <div className="flex items-center justify-center gap-10 p-6 flex-wrap">
+            <FormControl>
+              <Input
+                startDecorator={<SearchIcon />}
+                placeholder="Search"
+                variant='soft'
+                color='primary'
+                onChange={(e) => setSearchText(e.target.value)}
               />
-            </div>
+              <FormHelperText>Filter korvais by talam, ragam, and description.</FormHelperText>
+            </FormControl>
           </div>
         </div>
       </div>
-      <Korvai letters={example1} talam={'adi'} user={'sahasr'}/>
-      <Korvai letters={example2} talam={'adi'} user={'sahasr'}/>
-      <Korvai letters={example3} talam={'khanda chapu'} user={'sahasr'}/>
+      <div className='flex justify-center my-4'>
+        <Chip variant="contained" color="primary">
+          {filteredKorvais.length} result{filteredKorvais.length !== 1 && 's'}
+        </Chip>
+      </div>
+      {filteredKorvais.map((korvai, index) => (
+        <Korvai
+          key={index}
+          letters={korvai.letters}
+          talam={korvai.talam}
+          ragam={korvai.ragam}
+          description={korvai.description}
+        />
+      ))}
     </div>
-  )
+    </CssVarsProvider>
+  );
 }
--- src/components/Korvai.js
+++ src/components/Korvai.js
@@ -3,10 +3,11 @@ import { CssVarsProvider, extendTheme } from "@mui/joy/styles";
 import Chip from '@mui/joy/Chip';
 import Divider from '@mui/joy/Divider';
 import Icon from '@mdi/react';
-import { mdiAccountCircle } from '@mdi/js';
+// import { mdiAccountCircle } from '@mdi/js';
 import { mdiHandClap } from '@mdi/js';
+import { mdiAccountVoice } from '@mdi/js';
 
-const Korvai = ({ letters, talam }) => {
+const Korvai = ({ letters, talam, ragam, description }) => {
   const talamMap = new Map([
     ['adi', 16],
     ['khanda chapu', 10],
@@ -36,26 +37,31 @@ const Korvai = ({ letters, talam }) => {
       <div className="flex justify-center">
         <div className="m-4 bg-zinc-50 rounded-3xl w-3/4 drop-shadow-lg">
           <div className="p-6">
-            <div className="flex justify-between items-center pb-1">
+            {/* <div className="flex justify-between items-center pb-1">
               <div className="flex">
                 <Icon path={mdiAccountCircle} size={1} />
                 <div className="font-viet px-2">sahasr</div>
               </div>
-              {/* <div className="font-viet">date</div> */}
-            </div>
-            <div className="pb-1"></div>
-              <Chip color="neutral" variant="outlined" startDecorator={<Icon path={mdiHandClap} size={0.7} />}>
+              <div className="font-viet">date</div>
+            </div> */}
+              <Chip color="neutral" variant="outlined" startDecorator={<Icon path={mdiHandClap} size={0.7} />} sx={() => ({mx: '2px'})}>
                 {talam.replace(/\b\w/g, match => match.toUpperCase())}
               </Chip>
+              <Chip color="neutral" variant="outlined" startDecorator={<Icon path={mdiAccountVoice} size={0.7} />} sx={() => ({mx: '2px'})}>
+                {ragam.replace(/\b\w/g, match => match.toUpperCase())}
+              </Chip>
+            </div>
+            <div className="mx-6 font-viet">
+              {description}
             </div>
             <Divider sx={{m: 3}}/>
-            <div className="p-4 hidden md:block">
-              {chunkArray(letters.toUpperCase().split(''), talamMap.get(talam)).map((row, rowIndex) => (
+            <div className="p-4 hidden md:block pb-8">
+              {chunkArray(letters.replace(/\s/g, "").toUpperCase().split(''), talamMap.get(talam)).map((row, rowIndex) => (
                 <div key={rowIndex} className="flex">
                   {row.map((letter, columnIndex) => (
                     <div
                       key={columnIndex}
-                      className="border border-none p-2 flex-1 font-viet text-center"
+                      className="border border-none p-2 flex-1 font-swaram font-bold text-center"
                     >
                       {letter}
                     </div>
@@ -63,13 +69,13 @@ const Korvai = ({ letters, talam }) => {
                 </div>
               ))}
             </div>
-            <div className="p-4 block md:hidden">
-              {chunkArray(letters.toUpperCase().split(''), talamMap.get(talam) / 2).map((row, rowIndex) => (
+            <div className="p-4 block md:hidden pb-8">
+              {chunkArray(letters.replace(/\s/g, "").toUpperCase().split(''), talamMap.get(talam) / 2).map((row, rowIndex) => (
                 <div key={rowIndex} className="flex">
                   {row.map((letter, columnIndex) => (
                     <div
                       key={columnIndex}
-                      className="border border-none p-2 flex-1 font-viet text-center"
+                      className="border border-none p-2 flex-1 font-swaram font-bold text-center"
                     >
                       {letter}
                     </div>
